788143c9e0f415b09e322e07f47eed2161db556d
Heliosares <kyler.t.skeens@gmail.com>
2022-11-17 16:27:15 -0500

    Add helper getPlayer method

--- src/dev/heliosares/sync/SyncAPI.java
+++ src/dev/heliosares/sync/SyncAPI.java
@@ -1,75 +1,76 @@
 package dev.heliosares.sync;
 
-import java.io.IOException;
-import java.util.List;
-
 import dev.heliosares.sync.bungee.SyncBungee;
 import dev.heliosares.sync.net.NetListener;
 import dev.heliosares.sync.net.Packet;
 import dev.heliosares.sync.spigot.SyncSpigot;
 
+import java.io.IOException;
+import java.util.List;
+
 public class SyncAPI {
-	private static SyncCore instance;
+    private static SyncCore instance;
 
-	public static SyncCore getInstance() {
-		if (instance != null) {
-			return instance;
-		}
-		try {
-			if ((instance = SyncSpigot.getInstance()) != null) {
-				return instance;
-			}
-		} catch (Throwable ignored) {
-		}
-		try {
-			if ((instance = SyncBungee.getInstance()) != null) {
-				return instance;
-			}
-		} catch (Throwable ignored) {
-		}
-		return instance;
-	}
+    public static SyncCore getInstance() {
+        if (instance != null) {
+            return instance;
+        }
+        try {
+            if ((instance = SyncSpigot.getInstance()) != null) {
+                return instance;
+            }
+        } catch (Throwable ignored) {
+        }
+        try {
+            if ((instance = SyncBungee.getInstance()) != null) {
+                return instance;
+            }
+        } catch (Throwable ignored) {
+        }
+        return instance;
+    }
 
-	/**
-	 * Sends a packet to a specific server.
-	 * 
-	 * @param server The server to target. Must be contained within
-	 *               SyncAPI.getServers()
-	 * @param packet
-	 */
-	public static boolean send(String server, Packet packet) throws Exception {
-		return getInstance().getSync().send(server, packet);
-	}
+    /**
+     * Sends a packet to a specific server.
+     *
+     * @param server The server to target. Must be contained within
+     *               SyncAPI.getServers()
+     * @param packet
+     */
+    public static boolean send(String server, Packet packet) throws Exception {
+        return getInstance().getSync().send(server, packet);
+    }
 
-	/**
-	 * Sends a packet to the other endpoint. If executed from a server, goes to the
-	 * proxy. If executed from the proxy, goes to all servers.
-	 * 
-	 * @param packet
-	 * @throws IOException
-	 */
-	public static boolean send(Packet packet) throws Exception {
-		return getInstance().getSync().send(packet);
-	}
+    /**
+     * Sends a packet to the other endpoint. If executed from a server, goes to the
+     * proxy. If executed from the proxy, goes to all servers.
+     *
+     * @param packet
+     * @throws IOException
+     */
+    public static boolean send(Packet packet) throws Exception {
+        return getInstance().getSync().send(packet);
+    }
 
-	public static void register(NetListener listen) {
-		getInstance().getSync().getEventHandler().registerListener(listen);
-	}
+    public static void register(NetListener listen) {
+        getInstance().getSync().getEventHandler().registerListener(listen);
+    }
 
-	public static void unregister(NetListener listen) {
-		getInstance().getSync().getEventHandler().unregisterListener(listen);
-	}
+    public static void unregister(NetListener listen) {
+        getInstance().getSync().getEventHandler().unregisterListener(listen);
+    }
 
-	/**
-	 * Unregisters all listeners on this channel
-	 * 
-	 * @param channel
-	 */
-	public static void unregister(String channel) {
-		getInstance().getSync().getEventHandler().unregisterChannel(channel);
-	}
+    /**
+     * Unregisters all listeners on this channel
+     *
+     * @param channel
+     */
+    public static void unregister(String channel) {
+        getInstance().getSync().getEventHandler().unregisterChannel(channel);
+    }
 
-	public static List<String> getServers() {
-		return getInstance().getSync().getServers();
-	}
+    public static List<String> getServers() {
+        return getInstance().getSync().getServers();
+    }
+
 }
--- src/dev/heliosares/sync/net/UserManager.java
+++ src/dev/heliosares/sync/net/UserManager.java
@@ -1,236 +1,235 @@
 package dev.heliosares.sync.net;
 
-import java.io.IOException;
-import java.security.GeneralSecurityException;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.Iterator;
-import java.util.List;
-import java.util.Map;
-import java.util.Optional;
-import java.util.Map.Entry;
-import java.util.UUID;
-import java.util.stream.Collectors;
-
-import javax.annotation.Nullable;
-
+import dev.heliosares.sync.SyncCore;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import dev.heliosares.sync.SyncCore;
+import javax.annotation.Nullable;
+import java.io.IOException;
+import java.security.GeneralSecurityException;
+import java.util.*;
+import java.util.Map.Entry;
+import java.util.stream.Collectors;
 
 public class UserManager extends NetListener {
 
-	private final SyncNetCore sync;
-	private final SyncCore plugin;
-	private final Map<String, List<PlayerData>> players = new HashMap<>();
+    private final SyncNetCore sync;
+    private final SyncCore plugin;
+    private final Map<String, List<PlayerData>> players = new HashMap<>();
 
-	public UserManager(SyncCore plugin, SyncNetCore client) {
-		super(Packets.PLAYER_DATA.id, null);
-		this.sync = client;
-		this.plugin = plugin;
+    public UserManager(SyncCore plugin, SyncNetCore client) {
+        super(Packets.PLAYER_DATA.id, null);
+        this.sync = client;
+        this.plugin = plugin;
 
-		if (sync instanceof SyncClient)
-			plugin.scheduleAsync(() -> sendCurrentHash(), 10000, 10000);
-	}
+        if (sync instanceof SyncClient)
+            plugin.scheduleAsync(() -> sendCurrentHash(), 10000, 10000);
+    }
 
-	private int lasthash;
+    private int lasthash;
 
-	private void sendCurrentHash() {
-		List<PlayerData> pl = plugin.getPlayers();
-		if (pl.size() == 0) {
-			return;
-		}
-		int hash = hash(pl);
-		if (hash == lasthash) {
-			return;
-		}
-		try {
-			sync.send(new Packet(null, Packets.PLAYER_DATA.id, new JSONObject().put("hash", lasthash = hash)));
-		} catch (Exception e) {
-			plugin.print(e);
-		}
-	}
+    private void sendCurrentHash() {
+        List<PlayerData> pl = plugin.getPlayers();
+        if (pl.size() == 0) {
+            return;
+        }
+        int hash = hash(pl);
+        if (hash == lasthash) {
+            return;
+        }
+        try {
+            sync.send(new Packet(null, Packets.PLAYER_DATA.id, new JSONObject().put("hash", lasthash = hash)));
+        } catch (Exception e) {
+            plugin.print(e);
+        }
+    }
 
-	@Override
-	public void execute(String server, Packet packet) {
-		if (packet.getPayload().has("request")) {
-			if (packet.getPayload().getString("request").equalsIgnoreCase("all")) {
-				try {
-					sendPlayers(packet.getForward());
-				} catch (IOException | GeneralSecurityException e) {
-					plugin.print(e);
-					return;
-				}
-			}
-		} else if (packet.getPayload().has("players")) {
-			synchronized (players) {
-				players.put(packet.getForward(),
-						getPlayerData(packet.getForward(), packet.getPayload().getJSONArray("players")));
-			}
-		} else {
-			if (packet.getPayload().has("join")) {
-				List<PlayerData> add = getPlayerData(packet.getForward(), packet.getPayload().getJSONArray("join"));
-				List<PlayerData> current = players.get(packet.getForward());
-				if (current == null) {
-					current = add;
-				} else {
-					add.forEach(p -> quit(p.getServer(), p.getUUID()));
-					current.addAll(add);
-				}
-				synchronized (players) {
-					players.put(packet.getForward(), current);
-				}
-			} else if (packet.getPayload().has("quit")) {
-				packet.getPayload().getJSONArray("quit").toList().stream().map(o -> (String) o)
-						.forEach(uuid -> quit(packet.getForward(), UUID.fromString(uuid)));
-			} else {
-				return;
-			}
+    @Override
+    public void execute(String server, Packet packet) {
+        if (packet.getPayload().has("request")) {
+            if (packet.getPayload().getString("request").equalsIgnoreCase("all")) {
+                try {
+                    sendPlayers(packet.getForward());
+                } catch (IOException | GeneralSecurityException e) {
+                    plugin.print(e);
+                    return;
+                }
+            }
+        } else if (packet.getPayload().has("players")) {
+            synchronized (players) {
+                players.put(packet.getForward(),
+                        getPlayerData(packet.getForward(), packet.getPayload().getJSONArray("players")));
+            }
+        } else {
+            if (packet.getPayload().has("join")) {
+                List<PlayerData> add = getPlayerData(packet.getForward(), packet.getPayload().getJSONArray("join"));
+                List<PlayerData> current = players.get(packet.getForward());
+                if (current == null) {
+                    current = add;
+                } else {
+                    add.forEach(p -> quit(p.getServer(), p.getUUID()));
+                    current.addAll(add);
+                }
+                synchronized (players) {
+                    players.put(packet.getForward(), current);
+                }
+            } else if (packet.getPayload().has("quit")) {
+                packet.getPayload().getJSONArray("quit").toList().stream().map(o -> (String) o)
+                        .forEach(uuid -> quit(packet.getForward(), UUID.fromString(uuid)));
+            } else {
+                return;
+            }
 
-			if (packet.getPayload().has("hash")) {
-				int hash = packet.getPayload().getInt("hash");
-				int otherhash = 0;
-				try {
-					synchronized (players) {
-						otherhash = hash(players.get(packet.getForward()));
-					}
-				} catch (NullPointerException e) {
-				}
-				if (hash != otherhash) {
-					plugin.warning("Hash mismatch! " + hash + "!=" + otherhash);
-					request(packet.getForward());
-				}
-			}
-		}
-	}
+            if (packet.getPayload().has("hash")) {
+                int hash = packet.getPayload().getInt("hash");
+                int otherhash = 0;
+                try {
+                    synchronized (players) {
+                        otherhash = hash(players.get(packet.getForward()));
+                    }
+                } catch (NullPointerException e) {
+                }
+                if (hash != otherhash) {
+                    plugin.warning("Hash mismatch! " + hash + "!=" + otherhash);
+                    request(packet.getForward());
+                }
+            }
+        }
+    }
 
-	protected void request(String server) {
-		try {
-			sync.send(server, new Packet(null, Packets.PLAYER_DATA.id, new JSONObject().put("request", "all"))
-					.setForward(sync.getName()));
-		} catch (JSONException | IOException | GeneralSecurityException e) {
-			plugin.print(e);
-		}
-	}
+    protected void request(String server) {
+        try {
+            sync.send(server, new Packet(null, Packets.PLAYER_DATA.id, new JSONObject().put("request", "all"))
+                    .setForward(sync.getName()));
+        } catch (JSONException | IOException | GeneralSecurityException e) {
+            plugin.print(e);
+        }
+    }
 
-	public void sendPlayers(@Nullable String server) throws IOException, GeneralSecurityException {
-		if (sync instanceof SyncServer) {
-			return;
-		}
-		List<PlayerData> players = plugin.getPlayers();
-		sync.send(server,
-				new Packet(null, Packets.PLAYER_DATA.id,
-						new JSONObject()
-								.put("players",
-										new JSONArray(
-												players.stream().map(p -> p.toJSON()).collect(Collectors.toList())))
-								.put("hash", hash(players))));
-	}
+    public void sendPlayers(@Nullable String server) throws IOException, GeneralSecurityException {
+        if (sync instanceof SyncServer) {
+            return;
+        }
+        List<PlayerData> players = plugin.getPlayers();
+        sync.send(server,
+                new Packet(null, Packets.PLAYER_DATA.id,
+                        new JSONObject()
+                                .put("players",
+                                        new JSONArray(
+                                                players.stream().map(PlayerData::toJSON).collect(Collectors.toList())))
+                                .put("hash", hash(players))));
+    }
 
-	private static int hash(List<PlayerData> players) {
-		if (players == null || players.isEmpty()) {
-			return 0;
-		}
-		return players.stream().map(p -> p.hashData()).reduce((a, b) -> a + b).get();
-	}
+    private static int hash(List<PlayerData> players) {
+        if (players == null || players.isEmpty()) {
+            return 0;
+        }
+        return players.stream().map(PlayerData::hashData).reduce(Integer::sum).get();
+    }
 
-	private boolean quit(String server, UUID uuid) {
-		List<PlayerData> current = players.get(server);
-		if (current != null) {
-			Iterator<PlayerData> it = current.iterator();
-			for (PlayerData data; it.hasNext();) {
-				data = it.next();
-				if (data.getUUID().equals(uuid)) {
-					it.remove();
-					return true;
-				}
-			}
-		}
-		return false;
-	}
+    private boolean quit(String server, UUID uuid) {
+        List<PlayerData> current = players.get(server);
+        if (current != null) {
+            Iterator<PlayerData> it = current.iterator();
+            for (PlayerData data; it.hasNext(); ) {
+                data = it.next();
+                if (data.getUUID().equals(uuid)) {
+                    it.remove();
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
 
-	private static List<PlayerData> getPlayerData(String server, JSONArray arr) {
-		List<PlayerData> list = new ArrayList<>();
-		arr.forEach(o -> {
-			PlayerData data = getPlayerData(server, (JSONObject) o);
-			if (data != null)
-				list.add(data);
-		});
-		return list;
-	}
+    private static List<PlayerData> getPlayerData(String server, JSONArray arr) {
+        List<PlayerData> list = new ArrayList<>();
+        arr.forEach(o -> {
+            PlayerData data = getPlayerData(server, (JSONObject) o);
+            if (data != null)
+                list.add(data);
+        });
+        return list;
+    }
 
-	private static PlayerData getPlayerData(String server, JSONObject o) {
-		try {
-			return new PlayerData(server, o.getString("name"), o.getString("uuid"), o.getBoolean("v"));
-		} catch (JSONException e) {
-			return null;
-		}
-	}
+    private static PlayerData getPlayerData(String server, JSONObject o) {
+        try {
+            return new PlayerData(server, o.getString("name"), o.getString("uuid"), o.getBoolean("v"));
+        } catch (JSONException e) {
+            return null;
+        }
+    }
 
-	public Map<String, List<PlayerData>> getPlayers() {
-		Map<String, List<PlayerData>> out = new HashMap<>();
-		synchronized (players) {
-			players.forEach((k, v) -> out.put(k, Collections.unmodifiableList(v)));
-		}
-		return Collections.unmodifiableMap(out);
-	}
+    public Map<String, List<PlayerData>> getPlayers() {
+        Map<String, List<PlayerData>> out = new HashMap<>();
+        synchronized (players) {
+            players.forEach((k, v) -> out.put(k, Collections.unmodifiableList(v)));
+        }
+        return Collections.unmodifiableMap(out);
+    }
 
-	public String toFormattedString() {
-		synchronized (players) {
-			if (players.isEmpty()) {
-				return "No servers";
-			}
-			String build = "";
-			for (Entry<String, List<PlayerData>> entry : players.entrySet()) {
-				build += "§6§l" + entry.getKey() + "§7: ";
-				if (entry.getValue().isEmpty()) {
-					build += "None\n";
-					continue;
-				}
-				build += "\n";
-				String line = "";
-				for (PlayerData p : entry.getValue()) {
-					if (line.length() > 100) {
-						build += line + "\n";
-						line = "";
-					}
-					line += (p.isVanished() ? "§c[V]§7" : "§7") + p.getName() + ", ";
-				}
-				if (line.length() > 0) {
-					build += line + "\n";
-				}
-			}
-			return build.isEmpty() ? "" : build.substring(0, build.length() - 1);
-		}
-	}
+    public PlayerData getPlayer(String name) {
+        for (List<PlayerData> list : getPlayers().values()) {
+            Optional<PlayerData> o = list.stream().filter(d -> d.getName().equals(name)).findAny();
+            if (o.isPresent()) return o.get();
+        }
+        return null;
+    }
 
-	public void updatePlayer(PlayerData data) {
-		plugin.debug("Sending update for " + data.getName());
-		try {
-			sync.send("all", new Packet(null, Packets.PLAYER_DATA.id,
-					new JSONObject().put("join", new JSONArray().put(data.toJSON())).put("hash",
-							lasthash = plugin.getPlayers().stream()
-									.map(p -> p.getUUID().equals(data.getUUID()) ? data.hashData() : p.hashData())
-									.reduce((a, b) -> a + b).get())));
-		} catch (JSONException | IOException | GeneralSecurityException e) {
-			plugin.print(e);
-		}
-	}
+    public String toFormattedString() {
+        synchronized (players) {
+            if (players.isEmpty()) {
+                return "No servers";
+            }
+            String build = "";
+            for (Entry<String, List<PlayerData>> entry : players.entrySet()) {
+                build += "§6§l" + entry.getKey() + "§7: ";
+                if (entry.getValue().isEmpty()) {
+                    build += "None\n";
+                    continue;
+                }
+                build += "\n";
+                String line = "";
+                for (PlayerData p : entry.getValue()) {
+                    if (line.length() > 100) {
+                        build += line + "\n";
+                        line = "";
+                    }
+                    line += (p.isVanished() ? "§c[V]§7" : "§7") + p.getName() + ", ";
+                }
+                if (line.length() > 0) {
+                    build += line + "\n";
+                }
+            }
+            return build.isEmpty() ? "" : build.substring(0, build.length() - 1);
+        }
+    }
 
-	public void quitPlayer(UUID uuid) {
-		plugin.debug("Sending quit for " + uuid.toString());
-		try {
-			Optional<Integer> hash = plugin.getPlayers().stream().filter(p -> !p.getUUID().equals(uuid))
-					.map(p -> p.hashData()).reduce((a, b) -> a + b);
-			sync.send("all",
-					new Packet(null, Packets.PLAYER_DATA.id,
-							new JSONObject().put("quit", new JSONArray().put(uuid.toString())).put("hash",
-									hash.isPresent() ? (lasthash = hash.get()) : 0)));
-		} catch (JSONException | IOException | GeneralSecurityException e) {
-			plugin.print(e);
-		}
-	}
+    public void updatePlayer(PlayerData data) {
+        plugin.debug("Sending update for " + data.getName());
+        try {
+            sync.send("all", new Packet(null, Packets.PLAYER_DATA.id,
+                    new JSONObject().put("join", new JSONArray().put(data.toJSON())).put("hash",
+                            lasthash = plugin.getPlayers().stream()
+                                    .map(p -> p.getUUID().equals(data.getUUID()) ? data.hashData() : p.hashData())
+                                    .reduce((a, b) -> a + b).get())));
+        } catch (JSONException | IOException | GeneralSecurityException e) {
+            plugin.print(e);
+        }
+    }
+
+    public void quitPlayer(UUID uuid) {
+        plugin.debug("Sending quit for " + uuid.toString());
+        try {
+            Optional<Integer> hash = plugin.getPlayers().stream().filter(p -> !p.getUUID().equals(uuid))
+                    .map(p -> p.hashData()).reduce((a, b) -> a + b);
+            sync.send("all",
+                    new Packet(null, Packets.PLAYER_DATA.id,
+                            new JSONObject().put("quit", new JSONArray().put(uuid.toString())).put("hash",
+                                    hash.isPresent() ? (lasthash = hash.get()) : 0)));
+        } catch (JSONException | IOException | GeneralSecurityException e) {
+            plugin.print(e);
+        }
+    }
 }
